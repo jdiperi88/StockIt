1139d9a589e8eae406de6bb55aca82e27723796d
Joseph DiPeri <jdiperi88@gmail.com>
2017-09-15 00:46:44 -0400

    changin

--- client/src/reducers/auth_reducer.js
+++ client/src/reducers/auth_reducer.js
@@ -3,13 +3,13 @@
 //     UNAUTH_USER   
 // } from '../actions/types';
 
-// export default function(state = {}, action){
-//     switch(action,type) {
-//         case AUTH_USER:
-//             return {...state, authenticated: true};
-//         case UNAUTH_USER:
-//             return {...state, authenticated: false};
-//     }
+export default function(state = {}, action){
+    switch(action,type) {
+        case AUTH_USER:
+            return {...state, authenticated: true};
+        case UNAUTH_USER:
+            return {...state, authenticated: false};
+    }
 
-//     return state;
-// }
+    return state;
+}
