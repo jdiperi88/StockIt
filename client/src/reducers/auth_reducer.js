795a8a702d18332fcbbbea9872aa7e05b51bfd76
Joseph DiPeri <jdiperi88@gmail.com>
2017-09-15 00:45:06 -0400

    still broken

--- client/src/reducers/auth_reducer.js
+++ client/src/reducers/auth_reducer.js
@@ -1,15 +1,15 @@
-import {
-    AUTH_USER,
-    UNAUTH_USER   
-} from '../actions/types';
+// import {
+//     AUTH_USER,
+//     UNAUTH_USER   
+// } from '../actions/types';
 
-export default function(state = {}, action){
-    switch(action,type) {
-        case AUTH_USER:
-            return {...state, authenticated: true};
-        case UNAUTH_USER:
-            return {...state, authenticated: false};
-    }
+// export default function(state = {}, action){
+//     switch(action,type) {
+//         case AUTH_USER:
+//             return {...state, authenticated: true};
+//         case UNAUTH_USER:
+//             return {...state, authenticated: false};
+//     }
 
-    return state;
-}
+//     return state;
+// }
